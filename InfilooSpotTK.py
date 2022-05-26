09628b2650a4d10b3dedba3ed4f500b2a9640358
Uwe <Uwe.Beyer@gmx.de>
2022-05-26 21:46:37 +0200

    adding more UI elememts and enumerate playlists when selected

--- InfilooSpotTK.py
+++ InfilooSpotTK.py
@@ -9,15 +9,37 @@ from tkinter import ttk
 from tkinter.messagebox import showinfo
 
 
-def item_selected(event):
-    real_coords = (tree.winfo_pointerx() - tree.winfo_rootx(),
-                   tree.winfo_pointery() - tree.winfo_rooty())
-    item = tree.identify('item', *real_coords)
+''' combo_Type select serarch type - when Playlists are selected fetch them from spotify and fill combobox'''
+def cbType_Select(event):
+    if(cbType.get() == "Playlists"):
+        # Playlists is selcted      
+        cbSearch['state'] = 'readonly'                          # block edit
+        playlists   = sp.current_user_playlists(50, 0)          # fetch playlists from user account
 
-    for selected_item in tree.selection():
-        item = tree.item(selected_item)
+        list = []
+        cbSearch['values'] = playlists
+        for idx, item in enumerate(playlists['items']):
+            #     print(idx, item['name'] + " - " + item["id"])
+           list.append(item['name'])
+        cbSearch['values'] = list            
+    else:
+        # something we need t e´search is selected - list list
+        cbSearch['values']={}             # clear list if combobox again
+        cbSearch.set("")
+        cbSearch['state'] = 'normal'      # enable edit
+
+
+
+''' trResult_Select Item selected - use the colum to decide what to do'''
+def trResult_Select(event):
+    real_coords = (trResult.winfo_pointerx() - trResult.winfo_rootx(),
+                   trResult.winfo_pointery() - trResult.winfo_rooty())
+    item = trResult.identify('item', *real_coords)
+
+    for selected_item in trResult.selection():
+        item = trResult.item(selected_item)
         tags = item['tags']
-        col  = tree.identify_column(real_coords[0])
+        col  = trResult.identify_column(real_coords[0])
         if(col == "#1"):
             icol = 0
             type = "track:"
@@ -47,7 +69,7 @@ def doSearch(type, value, exact):
 
     # check if there is anything at all
     if(any(results)):
-        tree.delete(*tree.get_children())
+        trResult.delete(*trResult.get_children())
         trackURIs = []
         for idx, track in enumerate(results['tracks']['items']):
             # pprint(track)
@@ -59,7 +81,7 @@ def doSearch(type, value, exact):
             album_id      = track['album']['uri']
             album_name    = track['album']['name']
 
-            tree.insert('', tk.END, values=(track_name, artist_name, album_name), tags=(track_id, artist_id, album_id))
+            trResult.insert('', tk.END, values=(track_name, artist_name, album_name), tags=(track_id, artist_id, album_id))
             trackURIs.append(track['uri'])
 
         # sp.start_playback('4e6e703f564bfdfcb1c626ebd675b6f26ec90c7d', uris=trackURIs)   
@@ -82,8 +104,9 @@ btBack.pack(side=tk.LEFT, padx=5)
 cbType = ttk.Combobox(frame1)
 cbType['state'] = 'readonly'
 cbType.pack(side=tk.LEFT, padx=5, fill=tk.BOTH)
-cbType['values'] = ('Tilte', 'Artist', 'Album', 'Playlist')
+cbType['values'] = ('Tilte', 'Artist', 'Album', 'Playlists')
 cbType.current(newindex=0)
+cbType.bind("<<ComboboxSelected>>", cbType_Select)
 
 cbSearch = ttk.Combobox(frame1)
 cbSearch['state'] = 'normal'
@@ -93,16 +116,99 @@ btGo = tk.Button(frame1, text="Go", width=6)
 btGo.pack(side=tk.RIGHT, padx=5)
 
 ''' Result list and playlist '''
-columns = ('Titel', 'Artist', 'Album')
-tree = ttk.Treeview(root, columns=columns, show='headings')
-tree.heading('Titel',  text='Titel')
-tree.heading('Artist', text='Artist')
-tree.heading('Album',  text='Album')
-tree.pack(padx=10,pady=10,fill=tk.BOTH,expand=True)
-tree.bind('<<TreeviewSelect>>', item_selected)
+frame2 = tk.Frame(root)
+frame2.pack(fill=tk.X)
 
-scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=tree.yview)
-tree.configure(yscroll=scrollbar.set)
+frame21 = tk.Frame(frame2)
+frame21.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+columns = ('Titel', 'Artist', 'Album')
+trResult = ttk.Treeview(frame21, columns=columns, show='headings')
+trResult.heading('Titel',  text='Titel')
+trResult.heading('Artist', text='Artist')
+trResult.heading('Album',  text='Album')
+trResult.pack(padx=10,pady=10,side=tk.LEFT,expand=True)
+trResult.bind('<<TreeviewSelect>>', trResult_Select)
+
+frame22 = tk.Frame(frame2)
+frame22.pack(side=tk.RIGHT)
+
+btMinus = tk.Button(frame22, text="-", width=5)
+btMinus.pack(side=tk.TOP, padx=5)
+
+lbIndex = tk.Label(frame22, text="0", width=5)
+lbIndex.pack(side=tk.TOP, padx=5)
+
+btPlus = tk.Button(frame22, text="+", width=5)
+btPlus.pack(side=tk.BOTTOM, padx=5)
+
+''' Current playing and volume'''
+frame3 = tk.Frame(root)
+frame3.pack(fill=tk.X)
+
+frame31 = tk.Frame(frame3)
+frame31.pack(side=tk.LEFT)
+
+frame32 = tk.Frame(frame3)
+frame32.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+
+lbTitelLb = tk.Label(frame31, text="Titel:", width=5)
+lbTitelLb.pack(side=tk.TOP, padx=5)
+
+lbTitel = tk.Label(frame32, text="aktueller Titel", width=5)
+lbTitel.pack(side=tk.TOP, fill=tk.X, padx=5)
+
+
+lbArtistLb = tk.Label(frame31, text="Artist:", width=5)
+lbArtistLb.pack(side=tk.TOP, padx=5)
+
+lbArtist = tk.Label(frame32, text="aktueller Artist", width=5)
+lbArtist.pack(side=tk.TOP, fill=tk.X, padx=5)
+
+
+lbAlbumLb = tk.Label(frame31, text="Album:", width=5)
+lbAlbumLb.pack(side=tk.TOP, padx=5)
+
+lbAlbum = tk.Label(frame32, text="aktuelles Album", width=5)
+lbAlbum.pack(side=tk.TOP, fill=tk.X, padx=5)
+
+''' volume control'''
+frame33 = tk.Frame(frame3)
+frame33.pack(side=tk.RIGHT)
+
+btVolMinus = tk.Button(frame33, text="-", width=5)
+btVolMinus.pack(side=tk.TOP, padx=5)
+
+lbVol = tk.Label(frame33, text="0", width=5)
+lbVol.pack(side=tk.TOP, padx=5)
+
+btVolPlus = tk.Button(frame33, text="+", width=5)
+btVolPlus.pack(side=tk.BOTTOM, padx=5)
+
+''' Add to playlists'''
+frame4 = tk.Frame(root)
+frame4.pack(fill=tk.X)
+
+btP1 = tk.Button(frame4, text="-> P1", width=6)
+btP1.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5, pady=5)
+
+btP2 = tk.Button(frame4, text="-> P2", width=6)
+btP2.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5, pady=5)
+
+btP3 = tk.Button(frame4, text="-> P3", width=6)
+btP3.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5, pady=5)
+
+btP4 = tk.Button(frame4, text="-> P4", width=6)
+btP4.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5, pady=5)
+
+btP5 = tk.Button(frame4, text="-> P5", width=6)
+btP5.pack(side=tk.LEFT, fill=tk.X, expand=True, padx=5, pady=5)
+
+btQuit = tk.Button(frame4, text="Quit", width=5)
+btQuit.pack(side=tk.RIGHT, padx=5, pady=5)
+
+
 
 # init and connect to spotify
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="680ca5403c694cac9f37b459353cbaeb",         # infiloospot client id
